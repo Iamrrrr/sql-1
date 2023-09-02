d34fd9d733bf5b1a99e70996a6b28ae6f7df8d69
Iamrrrr <135818228+Iamrrrr@users.noreply.github.com>
2023-09-02 16:36:22 +0800

    P6.sql

--- src/P6.sql
+++ src/P6.sql
@@ -1,7 +1,9 @@
+-- Retrieve Employees with High Salary
 SELECT name,position,salary
 FROM employees
 WHERE salary>50000;
 
+-- Bonus
 Select name,position,salary
 FROM ( SELECT name,position,salary
        FROM employees
