5c62c5a11c68aba4e211be594aa5485e7b47c9fc
Iamrrrr <135818228+Iamrrrr@users.noreply.github.com>
2023-09-02 16:35:39 +0800

    P5.sql

--- src/P5.sql
+++ src/P5.sql
@@ -1,9 +1,10 @@
+-- Department Statistics
 SELECT department,avg(salary) as "Average Salary"
 FROM employees
 GROUP BY department
 ORDER BY avg(salary);
 
-
+-- Bonus
 SELECT department, salary,name
 FROM employees
 WHERE salary=(SELECT max(salary) FROM employees GROUP BY department);
