96b8ba910715e46e77c19f3de8baf42414a6a085
Iamrrrr <135818228+Iamrrrr@users.noreply.github.com>
2023-09-02 16:34:12 +0800

    P3.sql

--- src/P3.sql
+++ src/P3.sql
@@ -1,7 +1,9 @@
+-- Sales Analysis
 SELECT order_id,sum(quantity) as TOTAL_QUANTITY,avg(quantity) as AVERAGE_QUANTITY
 FROM sales
 GROUP BY order_id;
 
+-- Bonus
 SELECT order_id,sum(s.quantity*price) as TOTAL_SALES_AMT,avg(s.quantity*price) as AVG_SALES_AMT
 FROM sales s INNER JOIN
 inventories i ON 
